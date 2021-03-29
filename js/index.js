1ad0cbecce6bfb862510a29f08c1c9183bb57443
booldook <booldook@gmail.com>
2021-03-29 12:18:18 +0900

    ...

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-
+$(window).scroll(function() {
+	var scTop = $(this).scrollTop()
+	if(scTop > 200) {
+		if(!$('.header-wrapper').hasClass('active')) {
+			$('.header-wrapper').css('top', '-60px')
+			$('.header-wrapper').stop().animate({'top': 0}, 300)
+			$('.header-wrapper').addClass('active')
+		}
+	}
+	else {
+		$('.header-wrapper').removeClass('active')
+	}
+})
