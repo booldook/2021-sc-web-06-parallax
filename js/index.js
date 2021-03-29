0eab786be1735bb893455afc96492913aecb442a
booldook <booldook@gmail.com>
2021-03-29 13:23:54 +0900

    css로 애니메이션 적용

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(window).scroll(function() {
+/* $(window).scroll(function() {
 	var scTop = $(this).scrollTop()
 	if(scTop > 200) {
 		if(!$('.header-wrapper').hasClass('active')) {
@@ -8,6 +8,20 @@ $(window).scroll(function() {
 		}
 	}
 	else {
+		$('.header-wrapper').removeClass('active')
+	}
+}) */
+
+
+$(window).scroll(function () {
+	var scTop = $(this).scrollTop()
+	if (scTop > 200) {
+		$('.header-wrapper').css('transform', 'translateY(-100%)')
+		$('.header-wrapper').css('transform')
+		$('.header-wrapper').addClass('active')
+		$('.header-wrapper').css('transform', 'translateY(0)')
+	} else {
+		$('.header-wrapper').css('transform', 'translateY(0)')
 		$('.header-wrapper').removeClass('active')
 	}
 })
