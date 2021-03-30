97e210fdf380251c629483a2f054ff29f25a6f4a
booldook <booldook@gmail.com>
2021-03-30 14:38:59 +0900

    modal 완성

--- js/index.js
+++ js/index.js
@@ -29,3 +29,18 @@ $(window).scroll(function () {
 $('.mo-bars').click(function() {
 	$('.monavi-wrapper').toggleClass('active')
 })
+
+$('.modal-wrapper').click(function() {
+	$(this).css('display', 'none')
+})
+
+$('.pf-wrap > .pf > img').click(function() {
+	var src = $(this).attr('src')
+	var desc = $(this).attr('alt')
+	$('.modal-wrapper').css('display', 'block')
+	$('.modal-wrapper .modal-wrap').css('transform', 'scale(0)')
+	$('.modal-wrapper .modal-wrap').css('transform')
+	$('.modal-wrapper .modal-img').attr('src', src)
+	$('.modal-wrapper .modal-title').text(desc)
+	$('.modal-wrapper .modal-wrap').css('transform', 'scale(1)')
+})
