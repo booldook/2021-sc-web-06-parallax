995285ed9d1f6f93b4ba8b3b5984fd26ecd998d8
booldook <booldook@gmail.com>
2021-03-30 12:19:01 +0900

    animation 게임 완성

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,24 @@
-$('.box1').click(function() {
+/* $('.box1').click(function() {
 	$(this).toggleClass('active')
-});
+}); */
+
+var count = 0;
+var seconds = 0;
+
+$('.box1').click(function(){
+	count = count + 1;
+	$(this).css('animation-play-state', 'paused')
+	console.log(count)
+	if(count == 7) {
+		$('.modal-wrapper').css('display', 'table')
+		$('.score-wrap > span').text(seconds/1000)
+	}
+})
+
+$('#second').change(function(){
+	$('.box1').css('animation-duration', $(this).val())
+	$('.box1').css('animation-play-state', 'running')
+	setInterval(function() {
+		seconds = seconds + 1;
+	}, 1)
+})
